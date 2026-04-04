4e7c6cba193e2af3446c7e8381193cc0534cf3f0
Erick Díaz <erickdiazc717@gmail.com>
2026-04-04 12:10:48 -0500

    add login img animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,3 +57,4 @@ tl.to(
 gsap.defaults({ opacity: 0, y: -60, ease: 'power2.out', duration: 1.2 })
 gsap.from('.login__title', { delay: 2.5 })
 gsap.from('.login__form > *', { delay: 2.7, stagger: .2 })
+gsap.from('.login__img', { y: 0, x: 100, delay: 3.2, ease: 'elastic.out(1,0.6)' })
